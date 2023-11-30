3534955abf1644fa49fed537b107e0d3478694e0
Olesya <krayanskaya_ov@mail.ru>
2023-11-30 16:56:07 +0300

    controls conditional rendering

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -11,26 +11,40 @@ function Controls({onCartOpen, inCartList}) {
 
   let sum = inCartList
   .map((item) => item.count * item.good.price)
-  .reduce((partialSum, a) => partialSum + a, 0);  
+  .reduce((partialSum, a) => partialSum + a, 0); 
+  
+  if (inCartList.length > 0) {
+    return (
+      <div className="Controls">
+        <span className="Controls-text">В корзине: </span>
+        <span className="Controls-count">
+          {count.toLocaleString() +
+            ' ' +
+            plural(count, {
+              one: 'товар',
+              few: 'товара',
+              many: 'товаров',
+            }) +
+            ' /'}
+        </span>
+        <span className="Controls-sum">{sum.toLocaleString() + ' ₽'}</span>
+        <button className="Controls-btn" onClick={onCartOpen}>
+          Перейти
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Controls">
+        <span className="Controls-text">В корзине: </span>
+        <span className="Controls-empty">пусто</span>
+        <button className="Controls-btn" onClick={onCartOpen}>
+          Перейти
+        </button>
+      </div>
+    );
+  }
 
-  return (
-    <div className="Controls">
-      <span className="Controls-text">В корзине: </span>
-      <span className="Controls-count">
-        {count.toLocaleString() +
-          ' ' +
-          plural(count, {
-            one: 'товар',
-            few: 'товара',
-            many: 'товаров',
-          }) + ' /'}
-      </span>
-      <span className="Controls-sum">
-        {sum.toLocaleString() + ' ₽'}
-      </span>
-      <button className="Controls-btn" onClick={onCartOpen}>Перейти</button>
-    </div>
-  );
 }
 
 Controls.propTypes = {

cd635356c987942a84fc9251e1cc52db555197d7
Olesya <krayanskaya_ov@mail.ru>
2023-12-25 10:46:49 +0300

    navigation back to page

--- src/components/comments-login/index.js
+++ src/components/comments-login/index.js
@@ -3,12 +3,16 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './style.css';
 
-function CommentLogIn({ exists, type, onChangeLogInText }) {
+function CommentLogIn({ exists, type, onChangeLogInText, location }) {
   return (
     <>
       {!exists ? (
         <div className="CommentLogIn">
-          <Link className="CommentLogIn-link" to={'/login'}>
+          <Link
+            className="CommentLogIn-link"
+            to={'/login'}
+            state={{ back: location.pathname }}
+          >
             Войдите
           </Link>
           , чтобы иметь возможность{' '}
@@ -33,6 +37,7 @@ CommentLogIn.propTypes = {
   exists: PropTypes.bool,
   type: PropTypes.string,
   onChangeLogInText: PropTypes.func,
+  location: PropTypes.object,
 };
 
 export default memo(CommentLogIn);
